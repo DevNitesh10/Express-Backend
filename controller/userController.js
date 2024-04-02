5a85a193b577ee03d92179b035acf0aadeb5ac0e
DevNitesh10 <princenitesh325@gmail.com>
2024-04-02 10:56:06 +0545

    Address Model and User Controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,56 @@
 const UserModel = require('../models/userModel');
 const TokenModel = require('../models/tokenModel');
+const bcrypt = require('bcrypt')
 const sendEmail = require('../utils/emailSender');
 
-// 
+// register
+exports.register = async(req, res) => {
+    let {username, email, password, date_of_birth, gender, street, city, state, zipcode, country, country_code, phone } = req.body
+
+    // check if username already exists
+    let user = await UserModel.findOne({ username })
+    if (user){
+        return res.status(400).json({error: "Username already taken, choose another username"})
+
+    }
+
+    // check if email already used
+    user = await UserModel.findOne({ email })
+    if (user){
+        return res.status(400).json({error: "Email already taken, choose another email"})
+
+    }
+
+
+    // record the address -> _id
+    let address = await UserModel.create({
+        street, city, state, zipcode, country, country_code, phone
+    })
+
+    if(!address){
+        return res.status(400).json({ error: "Failed to register" });
+    }
+
+
+    // encrypt password
+    let salt = await bcrypt.gensalt(10)
+    let hashed_password = await bcrypt.hash(password, salt)
+
+    // register
+    let new_user = await UserModel.create({
+        username, 
+        email,
+        password: hashed_password,
+        gender,
+        date_of_birth,
+        address: address._id
+    })
+
+
+
+
+
+
 
 // generate token
 let token = await TokenModel.create({
@@ -24,3 +72,5 @@ if(!new_user){
     return res.status(400).json({ error: "Failed to register" });
 }
 res.send(new_user)
+
+}
---
+++ model/addressModel.js
@@ -0,0 +1,52 @@
+/* 
+street
+city
+state
+zipcode
+country
+country code
+phone
+*/
+
+const mongoose = require('mongoose');
+
+const addressSchema = new mongoose.Schema({
+    street: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    city: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    state: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    zipcode: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    country: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    countryCode: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {timestamps: true})
+
+
+module.exports = mongoose.model("Address", addressSchema)
