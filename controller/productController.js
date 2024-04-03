4e414defe802d8747e2f306ff9854d5a8b22c379
DevNitesh10 <princenitesh325@gmail.com>
2024-04-03 10:49:35 +0545

    Update and Delete Product

--- controller/productController.js
+++ controller/productController.js
@@ -47,3 +47,40 @@ exports.getProductByCategory = async (req, res) => {
     }
     res.send(products);
 }
+
+// update product
+exports.updateProduct = async (req, res) => {
+    try {
+        let product = await ProductModel.findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            price: req.body.price,
+            description: req.body.description,
+            category: req.body.category,
+            count_in_stock: req.body.count_in_stock,
+            product_image: req.file?.path
+        }, { new: true })
+        if (!product) {
+            return res.status(400).json({ error: "Something went wrong" })
+        }
+        res.send(product)
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+// delete Product 
+exports.deleteProduct = (req, res) => {
+    ProductModel.findByIdAndDelete(req.params.id)
+        .then(product => {
+            if (!product) {
+                return res.status(400).json({ error: "Product not found" })
+            }
+            else {
+                res.send({ message: "Product deleted Successfully" })
+            }
+        })
+        .catch(error =>
+            res.status(500).json({ error: error.message })
+        )
+}
